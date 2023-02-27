0b9561ea6878103ddbd594387301f666bed53a7c
Gabriel Simon Mbaiorga <msgtoolz@gmail.com>
2023-02-27 21:34:51 +0100

    clean domain model

--- src/app/models/blog-post.ts
+++ src/app/models/blog-post.ts
@@ -1,13 +1,23 @@
 export class BlogPost {
-    status?: string[];
-    _id?: string;
-    content?: string;
-    title?: string;
-    author?: string;
-    Created_date?: string;
+   public status: string[];
+    public id: string;
+    public content: string;
+    public title: string;
+    public author: string;
+    public createdDate: string;
 
     //assign vals from json to properties
-    constructor(values: Object = {}) { 
-        Object.assign(this, values);
+    // constructor(values: Object = {}) { 
+    //     Object.assign(this, values);
+    // }
+
+    public constructor(status?:string[],id?:string, content?:string,title?:string, author?:string, createdDate?:string ){
+        this.status = status || [];
+        this.id = id || '';
+        this.content = content || '';
+        this.title = title || '';
+        this.author = author || '';
+        this.createdDate = createdDate || '';
+
     }
 }
